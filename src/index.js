f6c853283c98839f62bfa3d692b34336c48b52e5
Benjamin Jenks <jenksb@gmail.com>
2017-10-28 19:03:06 +0100

    index.js: basic react router stubs added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
+import Login from './Login';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const customHistory = createBrowserHistory();
+
+const Root = () => (
+  <Router history={customHistory}>
+      <div>
+          <Route path="/login" component={Login}/>
+          <Route path="/app/home" component={App}/>
+          <Redirect from="/" to="/login"/>
+      </div>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();

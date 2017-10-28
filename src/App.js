4130e747c86691ab98d1c29dcf6774d1e04f366f
Benjamin Jenks <jenksb@gmail.com>
2017-10-28 12:21:37 +0100

    App.js: remove react generated noise

--- src/App.js
+++ src/App.js
@@ -118,13 +118,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image src={logo} height={80} width={120} alt="logo" />
         <SearchBar onSubmit={this.onSearch}/>
         <Visibility continuous onBottomVisible={this.nextSearchResultsPage}>
           <VideoGrid videos={this.state.videos}/>

54adadf46efb9d02bba990faf9955e8eb97fb3a9
Benjamin Jenks <jenksb@gmail.com>
2017-10-28 10:37:59 +0100

    App.js: extracted searching from gui component

--- src/App.js
+++ src/App.js
@@ -6,39 +6,59 @@ import youtubeSearch from 'youtube-search';
 import logo from './logo.svg';
 import './App.css';
 
+// youtube api
+
 const YOUTUBE_API_KEY = 'AIzaSyCtLy8fjvsD_KE8h-GMMoc0aHIBqJnmkpo';
 
-class SearchBar extends Component {
-  search = (e) => {
-    const term = e.target.term.value;
+class Youtube {
+  term = '';
+  videos = [];
+  pageInfo = {};
+  
+  search = (term, callback) => {
+    const query = {
+      term: term,
+      videos: [],
+      pageInfo: {},
+      callback: callback,
+      nextPage: () => {
+        const opts = {
+          key: YOUTUBE_API_KEY,
+          pageToken: query.pageInfo.nextPageToken
+        };
+        this._fetchPage(query, opts);
+      }
+    };
     const opts = {
       key: YOUTUBE_API_KEY
     };
-    this._youtube(term, opts, []);
+    this._fetchPage(query, opts);
   }
-  nextPage = () => {
-    const opts = {
-      key: YOUTUBE_API_KEY,
-      pageToken: this.state.pageInfo.nextPageToken
-    };
-    this._youtube(this.state.term, opts, this.state.videos);
-  }
-  _youtube = (term, opts, videos) => {
-    youtubeSearch(term, opts, (err, newVideos, pageInfo) => {
-      this.setState({
-        term: term,
-        videos: videos.concat(newVideos),
-        pageInfo: pageInfo
-      });
-      console.log(videos.concat(newVideos));
-      console.log(pageInfo);
+  _fetchPage = (query, opts) => {
+    youtubeSearch(query.term, opts, (err, newVideos, pageInfo) => {
+      query.videos = query.videos.concat(newVideos);
+      query.pageInfo = pageInfo;
+      query.callback(query, query.videos);
     });
   }
+}
+
+// gui
+
+class SearchBar extends Component {
   render() {
     return (
-      <Form onSubmit={this.search}>
+      <Form onSubmit={e => this.props.onSubmit(e.target.term.value)}>
         <Input placeholder='Search...' name='term' action={{icon:'search'}} />
       </Form>
+    );
+  }
+}
+
+class VideoGrid extends Component {
+  render() {
+    return (
+      <div/>
     );
   }
 }
@@ -52,6 +72,16 @@ class VideoPlayer extends Component {
 }
 
 class App extends Component {
+  youtube = new Youtube();
+  onSearch = (term) => {
+    console.log('Searching for ' + term);
+    this.youtube.search(term, (q, videos) => {  
+      this.setState({
+        query: q,
+        videos: videos
+      });
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -62,7 +92,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <SearchBar/>
+        <SearchBar onSubmit={this.onSearch}/>
         <VideoPlayer url="https://www.youtube.com/watch?v=MBVbVNgRmiA"/>
       </div>
     );

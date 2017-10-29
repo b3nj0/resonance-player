743b4c232b57b027a1b6fb46b0f4bdbd2d3eead3
Benjamin Jenks <jenksb@gmail.com>
2017-10-29 21:30:04 +0000

    Login.js: removed unused reference to fire

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import firebase from 'firebase';
 
-import fire from './fire';
 import fireauth from './fireauth';
 
 class Login extends Component {
